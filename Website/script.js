49015942f52714f0475d20ff7bb711e1bf38198c
Kauan Galdino <kauangaldino133@gmail.com>
2025-02-25 16:33:14 -0300

    Add files via upload

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+const addToCartButton = document.querySelector(
+  ".item-card-actions .btn-primary"
+);
+
+addToCartButton.addEventListener("click", () => {
+  console.log("Add to Cart button clicked!");
+});
